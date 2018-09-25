66e8c5e304b92a1230373a34b0dbd6de1ea8c23b
nemnous <nani.botta.123@msitprogram.net>
2018-09-25 21:43:10 +0530

    "check_style_score -> 18 of 24 passed. style score: 0/1 "

--- ADS1_Assignments/m2/CodeCamp_Percolation/CodeCamp_Percolation/Solution.java
+++ ADS1_Assignments/m2/CodeCamp_Percolation/CodeCamp_Percolation/Solution.java
@@ -10,36 +10,41 @@
 import java.util.Scanner;
 
 class Percolation {
+
 	private boolean[][] grid;
 	private int size;
 	private WeightedQuickUnionUF qUnion;
+
 	Percolation(int n) {
 		grid = new boolean[n][n];
 		qUnion = new WeightedQuickUnionUF(n * n + 2);
 		size = n;
 	}
+
 	public int myIndex(int row, int col) {
 		return row * size + col;
 	}
+
 	public void open(int row, int col) {
 
 		grid[row][col] = true;
 		// bottom
 		if (row + 1 < size && grid[row + 1][col]) {
-			qUnion.union(myIndex(row + 1, col), myIndex(row, col));
+			qUnion.union(myIndex(row, col), myIndex(row + 1, col));
 		}
 		// top
 		if (row - 1 >= 0 && grid[row - 1][col]) {
-			qUnion.union (myIndex(row - 1, col), myIndex(row, col));
+			qUnion.union (myIndex(row, col), myIndex(row - 1, col));
 		}
 		// left
 		if (col - 1 >= 0 && grid[row][col - 1]) {
-			qUnion.union(myIndex(row, col - 1), myIndex(row, col));
+			qUnion.union(myIndex(row, col), myIndex(row, col - 1));
 		}
 		// right
 		if (col + 1 < size && grid[row][col + 1]) {
-			qUnion.union(myIndex(row, col + 1), myIndex(row, col));
+			qUnion.union(myIndex(row, col), myIndex(row, col + 1));
 		}
+
 		if (row == 0) {
 			qUnion.union(myIndex(row, col), size * size);
 		}
