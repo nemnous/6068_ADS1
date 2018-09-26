f6f979b4094e00374e750f25d0524a8666382fdc
nemnous <nani.botta.123@msitprogram.net>
2018-09-27 03:53:53 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS1_Assignments/m3/a2/Sorted_Array/Sorted_Array/Solution.java
+++ ADS1_Assignments/m3/a2/Sorted_Array/Sorted_Array/Solution.java
@@ -8,12 +8,12 @@ class Solution {
 		int size2 = scan.nextInt();
 		scan.nextLine();
 		String[] str = (scan.nextLine() + "," + scan.nextLine()).split(",");
-		// int[] Array = new int[size1 + size2];
-		// for(int i = 0; i < size1 + size2; i++) {
-		// 	Array[i] = Integer.parseInt(str[i]);
-		// }
-		Arrays.sort(str);
-		String nani = String.join(" ", str);
+		int[] Array = new int[size1 + size2];
+		for(int i = 0; i < size1 + size2; i++) {
+			Array[i] = Integer.parseInt(str[i]);
+		}
+		Arrays.sort(Array);
+		String nani = Array.toString().replaceAll("[,/[/]]", "");
 		System.out.println(nani);
 	}
 }
