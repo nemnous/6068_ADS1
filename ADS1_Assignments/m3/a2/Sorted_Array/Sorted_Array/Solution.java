405ceeefb13751c8e2c36013b7d02100a3b5561e
nemnous <nani.botta.123@msitprogram.net>
2018-09-27 03:27:31 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS1_Assignments/m3/a2/Sorted_Array/Sorted_Array/Solution.java
+++ ADS1_Assignments/m3/a2/Sorted_Array/Sorted_Array/Solution.java
@@ -6,9 +6,10 @@ class Solution {
 		Scanner scan = new Scanner(System.in);
 		int size1 = scan.nextInt();
 		int size2 = scan.nextInt();
+		String[] str = (scan.nextLine() + "," + scan.nextLine()).split(",");
 		int[] Array = new int[size1 + size2];
 		for(int i = 0; i < size1 + size2; i++) {
-			Array[i] = scan.nextInt();
+			Array[i] = Integer.parseInt(str[i]);
 		}
 		Arrays.sort(Array);
 		System.out.println(Arrays.toString(Array));
