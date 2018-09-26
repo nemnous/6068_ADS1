739973785237bd46f098097c430f56664313d264
nemnous <nani.botta.123@msitprogram.net>
2018-09-26 19:29:27 +0530

    "check_style_score -> 24 of 24 passed. style score: 1/1 "

--- ADS1_Assignments/m2/CodeCamp_Percolation/CodeCamp_Percolation/Solution.java
+++ ADS1_Assignments/m2/CodeCamp_Percolation/CodeCamp_Percolation/Solution.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
-		/**
-		 * does nothing.
-		 */
-	}
-	/**
-	 * the main function reading input.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		// System.out.println(n);
-		Percolation percObj = new Percolation(n);
-		while (scan.hasNext()) {
-			percObj.open(scan.nextInt() - 1, scan.nextInt() - 1);
-		}
-		System.out.println(percObj.percolates());
-	}
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * does nothing.
+         */
+    }
+    /**
+     * the main function reading input.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        // System.out.println(n);
+        Percolation percObj = new Percolation(n);
+        while (scan.hasNext()) {
+            percObj.open(scan.nextInt() - 1, scan.nextInt() - 1);
+        }
+        System.out.println(percObj.percolates());
+    }
 }
