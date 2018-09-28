e9eb461f63f06aa7aec6907b8186d824669d13e7
nemnous <nani.botta.123@msitprogram.net>
2018-09-28 17:38:34 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS1_Assignments/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/Solution.java
+++ ADS1_Assignments/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/Solution.java
@@ -4,22 +4,31 @@ public class Solution {
 		Stack<Character> stk = new Stack<Character>();
 			Scanner scan = new Scanner(System.in);
 			int n = scan.nextInt();
+			// scan.nextLine();
 			while(n-- > 0) {
 				boolean check = true;
 				String str = scan.nextLine();
+				// System.out.println(str);
 				int len = str.length();
 				char[] charA= str.toCharArray();
+				// for(Character i : charA) {
+				// 	System.out.print(i + " ");
+				// }
 				for(int i = 0; i < len; i++) {
 					if(charA[i] == '[' || charA[i] == '(' ||charA[i] == '[')
 						stk.push(charA[i]);
+					// stk.print();
 					if(charA[i] == ']' || charA[i] == ')' || charA[i] == '}') {
-						if(stk.pop() != charA[i]) {
+						if(!stk.isEmpty() && stk.pop() != charA[i]) {
 							check = false;
 							break;
 						}
 					}
 				}
-				System.out.println(check);
+				String out;
+				if(check) out = "YES";
+				else out = "NO";
+				System.out.println(out);
 			}
 	}
 }
---
+++ ADS1_Assignments/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/nani.java
@@ -0,0 +1,12 @@
+class nani {
+	public static void main(String[] args) {
+		Stack<Character> stk = new Stack<Character>();
+		stk.push('n');
+		stk.push('a');
+		stk.print();
+		System.out.println();
+		stk.pop();
+		stk.print();
+
+	}
+}
