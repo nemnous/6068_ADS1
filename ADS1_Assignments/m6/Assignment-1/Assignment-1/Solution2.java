ee11ba0d4ccdb52a8640f488b67e0f8bb2febe69
nemnous <nani.botta.123@msitprogram.net>
2018-09-29 23:27:43 +0530

    "check_style_score -> 12 of 12 passed. style score: 0/1 "

---
+++ ADS1_Assignments/m6/Assignment-1/Assignment-1/Solution2.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Stack;
+
+class AddLargeNumbers {
+    
+    public static LinkedList numberToDigits(String number) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        char[] charA = number.toCharArray();
+        for (Character i : charA) {
+            list.add(Character.getNumericValue(i));
+        }
+        return list;
+
+    }
+
+    public static String digitsToNumber(LinkedList list) {
+        return list.toString().replaceAll("\\[", "").replaceAll("]", "").replaceAll(", ", "");
+    }
+
+    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        Stack<Integer> stk1 = new Stack<Integer>();
+        Stack<Integer> stk2 = new Stack<Integer>();
+        Stack<Integer> stk3 = new Stack<Integer>();
+
+        for (int i = 0; i < list1.size(); i++) {
+            int a = (Integer) (list1.get(i));
+            stk1.push(a);
+        }
+        for (int i = 0; i < list2.size(); i++) {
+            int a = (Integer) (list2.get(i));
+            stk2.push(a);
+        }
+        int carry = 0;
+        final int ten = 10;
+        while (!stk1.isEmpty() && !stk2.isEmpty()) {
+            int a = stk1.pop() + stk2.pop() + carry;
+            stk3.push(a % ten);
+            carry = a / ten;
+
+        }
+        int l1 = list1.size();
+        int l2 = list2.size();
+        if (l2 < l1) {
+            int t = l1;
+            l1 = l2;
+            l2 = l1;
+            Stack<Integer> tstk = stk1;
+            stk2 = stk1;
+            stk1 = tstk;
+        }
+        if (l1 < l2) {
+            for (int i = l1; i < l2; i++) {
+                int a = stk2.pop() + carry;
+                stk3.push(a % ten);
+                carry = a / ten;
+            }
+        }
+        if (carry > 0) {
+            stk3.push(carry);
+        }
+        while (!stk3.isEmpty()) {
+            list.add(stk3.pop());
+        }
+        return list;
+    }
+}
+
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String p = sc.nextLine();
+        String q = sc.nextLine();
+        switch(input){
+            case "numberToDigits":
+                LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
+                LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
+                System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+                System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+                break;
+
+            case "addLargeNumbers":
+                pDigits = AddLargeNumbers.numberToDigits(p);
+                qDigits = AddLargeNumbers.numberToDigits(q);
+                LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+                System.out.println(AddLargeNumbers.digitsToNumber(result));
+                break;
+        }
+    }
+    
+}
---
+++ ADS1_Assignments/m6/Assignment-1/Assignment-1/test.java
@@ -0,0 +1,9 @@
+class test {
+	public static void main(String[] args) {
+		Stack<Integer> stk = new Stack<Integer>();
+		stk.push(1);
+		stk.push(2);
+		stk.print();
+
+	}
+}
