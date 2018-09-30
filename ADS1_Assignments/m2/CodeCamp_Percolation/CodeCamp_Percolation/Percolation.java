483126745bd32ccd561eb06f8fd9a07d1ced657e
nemnous <nani.botta.123@msitprogram.net>
2018-09-30 22:03:14 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS1_Assignments/m2/CodeCamp_Percolation/CodeCamp_Percolation/Percolation.java
+++ ADS1_Assignments/m2/CodeCamp_Percolation/CodeCamp_Percolation/Percolation.java
@@ -1,52 +1,138 @@
 import java.util.Scanner;
-public class Solution{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int size = Integer.parseInt(sc.nextLine());
-		percolate obj = new percolate(size);
-		while(sc.hasNextLine()){
-			String[] input = sc.nextLine().split(" ");
-			obj.open(Integer.parseInt(input[0])-1, Integer.parseInt(input[1])-1);
-		}
-			System.out.println(obj.ispercolate());
-	}
-}
-
-
-class percolate{
-	int size;
-	boolean[][] grid;
-	WeightedQuickUnionUF obj;
-
-	percolate(int size){
-		this.size = size;
-		grid = new boolean[size][size];
-		obj = new WeightedQuickUnionUF(size*size + 2);
-	}
-
-	public void open(int i, int j){
-		if(grid[i][j]) return;
-		grid[i][j] = true;
-		if(i == 0){
-			obj.union(convert(i, j), size*size);
-		}if(i == size-1){
-			obj.union(convert(i, j), size*size+1);
-		}
-		if(i < size - 1 && grid[i+1][j]){
-			obj.union(convert(i,j), convert(i+1,j));
-		}if(i > 0 && grid[i-1][j]){
-			obj.union(convert(i, j), convert(i-1,j));
-		}if(j > 0 && grid[i][j-1]){
-			obj.union(convert(i, j), convert(i,j-1));
-		}if(j < size-1 && grid[i][j+1]){
-			obj.union(convert(i, j), convert(i,j+1));
-		}
-	}
-	public boolean ispercolate(){
-		return obj.connected(size*size, size*size+1);
-	}
-
-	public int convert(int i, int j){
-		return ((i * size) + j);
-	}
+/**
+ * Class for percolation.
+ */
+class Percolation {
+    /**
+     * declaration of weightqUnion.
+     */
+    private WeightedQuickUnionUF weightqUnion;
+    /**
+     * private declaration of n.
+     */
+    private int n;
+    /**
+     * declaration of size.
+     */
+    private int size;
+    /**
+     * declaration of top.
+     */
+    private int top;
+    /**
+     * declaration of bottom.
+     */
+    private int bottom;
+    /**
+     * declaration of count.
+     */
+    private int count;
+    /**
+     * declaration of boolean array.
+     */
+    private boolean[] connected;
+    /**
+     * Constructs the object.
+     *
+     * @param      n1    The n 1
+     */
+    Percolation(final int n1) {
+        this.n = n1;
+        this.size = n1 * n1;
+        this.top = size;
+        this.bottom = size + 1;
+        this.count = 0;
+        weightqUnion = new WeightedQuickUnionUF(size + 2);
+        connected = new boolean[size];
+        for (int i = 0; i < n; i++) {
+            weightqUnion.union(top, i);
+            weightqUnion.union(bottom, size - i - 1);
+        }
+    }
+    /**
+     * converts to 1D array.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private int convert(final int i, final int j) {
+//since we take size as 0 to n-1, we decrement one value from rows and columns.
+        return n * (i - 1) + (j - 1);
+    }
+    /**
+     * Connects open sites(== full site).
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
+    private void connectOpenSites(final int row, final int col) {
+        if (connected[col] && !weightqUnion.connected(row, col)) {
+            weightqUnion.union(row, col);
+        }
+    }
+    /**
+     * opens the blocked sites.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
+    public void open(final int row, final int col) {
+        // int count = 0;
+        int index = convert(row, col);
+        connected[index] = true;
+        count++;
+        int toprow = index - n;
+        int bottomrow = index + n;
+        if (n == 1) {
+            weightqUnion.union(top, index);
+            weightqUnion.union(bottom, index);
+        }
+        if (bottomrow < size) { //bottom
+            connectOpenSites(index, bottomrow);
+        }
+        if (toprow >= 0) { //top
+            connectOpenSites(index, toprow);
+        }
+        if (col == 1) { //left
+            if (col != n) {
+                connectOpenSites(index, index + 1);
+            }
+            return;
+        }
+        if (col == n) { //right
+            connectOpenSites(index, index - 1);
+            return;
+        }
+        connectOpenSites(index, index + 1);
+        connectOpenSites(index, index - 1);
+    }
+    /**
+     * Determines if it is an open site.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if open, False otherwise.
+     */
+    public boolean isOpen(final int row, final int col) {
+        return connected[convert(row, col)];
+    }
+    /**
+     * counts number of open sites.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int findOpenSites() {
+        return count;
+    }
+    /**
+     * returns true if percolates.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean percolates() {
+        return weightqUnion.connected(top, bottom);
+    }
 }
--- ADS1_Assignments/m2/CodeCamp_Percolation/CodeCamp_Percolation/Solution.java
+++ ADS1_Assignments/m2/CodeCamp_Percolation/CodeCamp_Percolation/Solution.java
@@ -1,52 +1,28 @@
 import java.util.Scanner;
-class Solution{
-    public static void main(String[] args) {
+/**
+ * class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not used
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        // WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
         Scanner sc = new Scanner(System.in);
-        int size = Integer.parseInt(sc.nextLine());
-        percolate obj = new percolate(size);
-        while(sc.hasNextLine()){
-            String[] input = sc.nextLine().split(" ");
-            obj.open(Integer.parseInt(input[0])-1, Integer.parseInt(input[1])-1);
+        int n = Integer.parseInt(sc.nextLine());
+        Percolation p = new Percolation(n);
+        while (sc.hasNext()) {
+            String[] tokens = sc.nextLine().split(" ");
+            p.open(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
         }
-            System.out.println(obj.ispercolate());
-    }
-}
-
-
-class percolate{
-    int size;
-    boolean[][] grid;
-    WeightedQuickUnionUF obj;
-
-    percolate(int size){
-        this.size = size;
-        grid = new boolean[size][size];
-        obj = new WeightedQuickUnionUF(size*size + 2);
-    }
-
-    public void open(int i, int j){
-        if(grid[i][j]) return;
-        grid[i][j] = true;
-        if(i == 0){
-            obj.union(convert(i, j), size*size);
-        }if(i == size-1){
-            obj.union(convert(i, j), size*size+1);
-        }
-        if(i < size - 1 && grid[i+1][j]){
-            obj.union(convert(i,j), convert(i+1,j));
-        }if(i > 0 && grid[i-1][j]){
-            obj.union(convert(i, j), convert(i-1,j));
-        }if(j > 0 && grid[i][j-1]){
-            obj.union(convert(i, j), convert(i,j-1));
-        }if(j < size-1 && grid[i][j+1]){
-            obj.union(convert(i, j), convert(i,j+1));
-        }
-    }
-    public boolean ispercolate(){
-        return obj.connected(size*size, size*size+1);
-    }
-
-    public int convert(int i, int j){
-        return ((i * size) + j);
+        System.out.println(p.percolates() && p.findOpenSites() != 0);
     }
 }
