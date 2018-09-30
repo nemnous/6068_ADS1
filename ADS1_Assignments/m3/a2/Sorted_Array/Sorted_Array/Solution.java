5a7870e2f990fe3bdc1e76d8b7e79282343eb199
nemnous <nani.botta.123@msitprogram.net>
2018-09-30 22:23:39 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- ADS1_Assignments/m3/a2/Sorted_Array/Sorted_Array/Solution.java
+++ ADS1_Assignments/m3/a2/Sorted_Array/Sorted_Array/Solution.java
@@ -1,25 +1,38 @@
 import java.util.Scanner;
 import java.util.Arrays;
-
-class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int size1 = scan.nextInt();
-		int size2 = scan.nextInt();
-		String[] str;
-		scan.nextLine();
-		if (size1 != 0)
-			str = (scan.nextLine() + "," + scan.nextLine()).split(",");
-		else {
-			scan.nextLine();
-			str = scan.nextLine().split(",");
-		}
-		int[] Array = new int[size1 + size2];
-		for(int i = 0; i < size1 + size2; i++) {
-			Array[i] = Integer.parseInt(str[i]);
-		}
-		Arrays.sort(Array);
-		String nani = Arrays.toString(Array).replaceAll("[^0-9,]", "");
-		System.out.println(nani);
-	}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // Unused
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size1 = scan.nextInt();
+        int size2 = scan.nextInt();
+        String[] str;
+        scan.nextLine();
+        if (size1 != 0) {
+str = (scan.nextLine() + "," + scan.nextLine()).split(",");
+        } else {
+            scan.nextLine();
+            str = scan.nextLine().split(",");
+        }
+        int[] array = new int[size1 + size2];
+        for (int i = 0; i < size1 + size2; i++) {
+            array[i] = Integer.parseInt(str[i]);
+        }
+        Arrays.sort(array);
+    String nani = Arrays.toString(array).replaceAll("[^0-9,]", "");
+        System.out.println(nani);
+    }
 }
