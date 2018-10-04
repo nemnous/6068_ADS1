d628094991ce18667cd91e08524fddd76f5e3f91
nemnous <nani.botta.123@msitprogram.net>
2018-10-04 10:18:31 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- ADS1_Assignments/m3/a2/Sorted_Array/Sorted_Array/Solution.java
+++ ADS1_Assignments/m3/a2/Sorted_Array/Sorted_Array/Solution.java
@@ -15,9 +15,8 @@ final class SelectionSort {
      * @param arr : array
      * @return sorted array
      */
-    public static int[] sort(final int arr[]) {
+    public static int[] sort(final int[] arr) {
         int n = arr.length;
-
         // One by one move boundary of unsorted subarray
         for (int i = 0; i < n - 1; i++) {
             // Find the minimum element in unsorted array
