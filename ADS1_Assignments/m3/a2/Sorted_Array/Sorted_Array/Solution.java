72cd803a9ddb5cf199e2989d7166c2991158221f
nemnous <nani.botta.123@msitprogram.net>
2018-10-04 10:07:48 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS1_Assignments/m3/a2/Sorted_Array/Sorted_Array/Solution.java
+++ ADS1_Assignments/m3/a2/Sorted_Array/Sorted_Array/Solution.java
@@ -36,18 +36,39 @@ str = (scan.nextLine() + "," + scan.nextLine()).split(",");
     String nani = Arrays.toString(array).replaceAll("[^0-9,]", "");
         System.out.println(nani);
     }
-    public static int[] sort(final int[] array) {
-        for(int i : array) {
-            int min = i;
-            for(int j : array) {
-                if(min>j) {
-                    min = j;
-                    int t = i;
-                    i = j;
-                    j = t;
-                }
-            }
+    // public static int[] sort(final int[] array) {
+    //     for(int i : array) {
+    //         int min = i;
+    //         for(int j : array) {
+    //             if(min>j) {
+    //                 min = j;
+    //                 int t = i;
+    //                 i = j;
+    //                 j = t;
+    //             }
+    //         }
+    //     }
+    //     return array;
+    //     }
+    static int[] sort(int arr[]) 
+    { 
+        int n = arr.length; 
+  
+        // One by one move boundary of unsorted subarray 
+        for (int i = 0; i < n-1; i++) 
+        { 
+            // Find the minimum element in unsorted array 
+            int min_idx = i; 
+            for (int j = i+1; j < n; j++) 
+                if (arr[j] < arr[min_idx]) 
+                    min_idx = j; 
+  
+            // Swap the found minimum element with the first 
+            // element 
+            int temp = arr[min_idx]; 
+            arr[min_idx] = arr[i]; 
+            arr[i] = temp; 
         }
-        return array;
-        }
+        return arr;
+    }
 }
