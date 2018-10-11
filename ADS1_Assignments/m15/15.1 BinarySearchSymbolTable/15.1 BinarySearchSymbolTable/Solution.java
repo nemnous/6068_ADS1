bb489a0efd4ff538b131efa1c5dd24561a6d1180
nemnous <nani.botta.123@msitprogram.net>
2018-10-11 17:09:27 +0530

    "ads1_m15_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1_Assignments/m15/15.1 BinarySearchSymbolTable/15.1 BinarySearchSymbolTable/Solution.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * main method to drive the program.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        BinarySearchST<String, Integer> bs = new BinarySearchST<String,
+        Integer>(input.length);
+        for (int i = 0; i < input.length; i++) {
+            bs.put(input[i], i);
+        }
+        while (sc.hasNext()) {
+            String[] tokens = sc.nextLine().split(" ");
+            switch (tokens[0]) {
+            case "max":
+                System.out.println(bs.max());
+                break;
+            case "floor":
+                System.out.println(bs.floor(tokens[1]));
+                break;
+            case "rank":
+                System.out.println(bs.rank(tokens[1]));
+                break;
+            case "deleteMin":
+                bs.deleteMin();
+                break;
+            case "contains":
+                System.out.println(bs.contains(tokens[1]));
+                break;
+            case "keys":
+                for (String s: bs.keys()) {
+                    System.out.println(s + " " + Integer.toString(bs.get(s)));
+                }
+                //System.out.println(bs.keys());
+                break;
+            case "get":
+            System.out.println(bs.get(tokens[1]));
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
