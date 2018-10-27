f3a611f4e0f2978e1338f25a4824a98ee9190f1c
nemnous <nani.botta.123@msitprogram.net>
2018-10-27 13:08:17 +0530

    "m24_a2_1: 3/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1_Assignments/m24/2/Solution.java
+++ ADS1_Assignments/m24/2/Solution.java
@@ -53,13 +53,23 @@ class Student {
     }
 
     public String toString() {
-        return name +" " + rollNumber + " " + marks;
+        return name + " " + rollNumber + " " + marks;
     }
 }
-
+/**
+ * in this section I want to take marks as key
+ * and Student array as an integer.
+ * so that  students having same marks can be stored in an array.
+ *
+ */
 final class Solution {
-
-    public static void main(String[] args) {
+    /**
+     * this is the maih function all the inputs are taken from the console.
+     * and the output i sprinted.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         BinarySearchST<Double, Student[]> binST
             = new BinarySearchST<Double, Student[]>();
 
@@ -68,19 +78,18 @@ final class Solution {
         int testCases = scan.nextInt();
         scan.nextLine();
         Double[] stArr = new Double[testCases];
-        int index= 0;
+        int index = 0;
 
         for (int i = 0; i < testCases; i++) {
             String[] temp = scan.nextLine().split(",");
             final Double marks = Double.parseDouble(temp[2]);
 
-Student stu = new Student(Integer.parseInt(temp[0]), temp[1], marks);
-            if(!binST.contains(marks)) {
+            Student stu = new Student(Integer.parseInt(temp[0]), temp[1], marks);
+            if (!binST.contains(marks)) {
                 Student[] stuArr = new Student[2];
                 stuArr[0] = stu;
                 binST.put(marks, stuArr);
-            }
-            else {
+            } else {
                 binST.get(marks)[1] = stu;
             }
         }
@@ -88,32 +97,37 @@ Student stu = new Student(Integer.parseInt(temp[0]), temp[1], marks);
 
         int noQuery = scan.nextInt();
         scan.nextLine();
-        for(int i = 0; i < noQuery; i++) {
+        for (int i = 0; i < noQuery; i++) {
             String[] str = scan.nextLine().split(" ");
 
-            switch(str[0]) {
-                case "BE":
-                    Double t1 = Double.parseDouble(str[1]);
-                    Double t2 = Double.parseDouble(str[2]);
-                    for(Double d : binST.keys(t1, t2)) {
-                        System.out.println(binST.get(d)[0].getName());
-                        if(binST.get(d)[1] != null)
-                            System.out.println(binST.get(d)[1].getName());
-                    }
-                    break;
-                // case "LE":
-                //     for(String d : binST.keys()) {
-                //         if(binST.get(d).getMarks() <= Double.parseDouble(str[1]))
-                //         System.out.println(binST.get(d).getName());
-                //     }
-                //     break;
-                // case "GE":
+            switch (str[0]) {
+            case "BE":
+                Double t1 = Double.parseDouble(str[1]);
+                Double t2 = Double.parseDouble(str[2]);
+                for (Double d : binST.keys(t1, t2)) {
+                    System.out.println(binST.get(d)[0].getName());
+                    if (binST.get(d)[1] != null)
+                        System.out.println(binST.get(d)[1].getName());
+                }
+                break;
+            case "LE":
+                for (Double d : binST.keys(binST.min(), Double.parseDouble(str[1]))) {
+                    // System.out.println(Arrays.toString(binSt.get(d));
+                    System.out.println(binST.get(d)[0].getName());
+                    if (binST.get(d)[1] != null)
+                        System.out.println(binST.get(d)[1].getName());
+                }
+                break;
 
-                //     for(String d : binST.keys()) {
-                //         if(binST.get(d).getMarks() >= Double.parseDouble(str[1]))
-                //             System.out.println(binST.get(d).getName());
-                //     }
-                //     break;
+            case "GE":
+
+                for (Double d : binST.keys(Double.parseDouble(str[1]), binST.max())) {
+                    // System.out.println(Arrays.toString(binSt.get(d));
+                    System.out.println(binST.get(d)[0].getName());
+                    if (binST.get(d)[1] != null)
+                        System.out.println(binST.get(d)[1].getName());
+                }
+                break;
             }
 
         }
