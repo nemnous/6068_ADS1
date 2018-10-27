b2964307ca37104a18f72ad141ad2d6d120d336d
nemnous <nani.botta.123@msitprogram.net>
2018-10-27 10:33:00 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1_Assignments/m24/1/Solution.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+class Student {
+	String name;
+	int rollNumber;
+	Double marks;
+
+	Student(int rollNumber, String name, Double marks) {
+		this.name = name;
+		this.rollNumber = rollNumber;
+		this.marks = marks;
+	} 
+}
+
+
+
+class Solution {
+	public static void main(String[] args) {
+		LinearProbingHashST<Integer, Student > linearST = new LinearProbingHashST<Integer, Student>();
+
+		Scanner scan = new Scanner(System.in);
+
+		int testCases = scan.nextInt();
+
+		for(int i = 0; i < testCases; i++) {
+			String[] temp = scan.nextLine().split(",");
+
+			Student stu = new Student(Integer.parseInt(temp[0]), temp[1], Double.parseDouble(temp[2]));
+
+			linearST.put(Integer.parseInt(temp[0]), stu);
+
+		}
+
+		int noQuery = scan.nextInt();
+
+		for(int i = 0; i < noQuery; i++) {
+			String[] query = scan.nextLine().split(" ");
+			
+			switch(query[2]) {
+				case "1":
+					System.out.println(linearST.get(Integer.parseInt(query[1])).name);
+					break;
+				case "2":
+					System.out.println(linearST.get(Integer.parseInt(query[1])).marks);
+
+			}
+		}
+	}
+}
