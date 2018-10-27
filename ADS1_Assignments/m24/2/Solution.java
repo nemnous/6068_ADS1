b6be0609ec16fed6d27f60f2b62fd78d3a488836
nemnous <nani.botta.123@msitprogram.net>
2018-10-27 12:47:33 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1_Assignments/m24/2/Solution.java
+++ ADS1_Assignments/m24/2/Solution.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.*;
+
 class Student {
     /**
      * variables name, roll, marks.
@@ -49,27 +51,40 @@ class Student {
     int getRoll() {
         return this.rollNumber;
     }
+
+    public String toString() {
+        return name +" " + rollNumber + " " + marks;
+    }
 }
 
 final class Solution {
 
     public static void main(String[] args) {
-        BinarySearchST<Double, Student> binST
-            = new BinarySearchST<Double, Student>();
+        BinarySearchST<Double, Student[]> binST
+            = new BinarySearchST<Double, Student[]>();
 
         Scanner scan = new Scanner(System.in);
 
         int testCases = scan.nextInt();
         scan.nextLine();
+        Double[] stArr = new Double[testCases];
+        int index= 0;
 
         for (int i = 0; i < testCases; i++) {
             String[] temp = scan.nextLine().split(",");
             final Double marks = Double.parseDouble(temp[2]);
 
 Student stu = new Student(Integer.parseInt(temp[0]), temp[1], marks);
-
-            binST.put(marks, stu);
+            if(!binST.contains(marks)) {
+                Student[] stuArr = new Student[2];
+                stuArr[0] = stu;
+                binST.put(marks, stuArr);
+            }
+            else {
+                binST.get(marks)[1] = stu;
+            }
         }
+
 
         int noQuery = scan.nextInt();
         scan.nextLine();
@@ -81,19 +96,24 @@ Student stu = new Student(Integer.parseInt(temp[0]), temp[1], marks);
                     Double t1 = Double.parseDouble(str[1]);
                     Double t2 = Double.parseDouble(str[2]);
                     for(Double d : binST.keys(t1, t2)) {
-                        System.out.println(binST.get(d).getName());
+                        System.out.println(binST.get(d)[0].getName());
+                        if(binST.get(d)[1] != null)
+                            System.out.println(binST.get(d)[1].getName());
                     }
                     break;
-                case "LE":
-                    for(Double d : binST.keys(0.0, Double.parseDouble(str[1]))) {
-                        System.out.println(binST.get(d).getName());
-                    }
-                    break;
-                case "GE":
-                    for(Double d : binST.keys(Double.parseDouble(str[1]), 100.0)) {
-                        System.out.println(binST.get(d).getName());
-                    }
-                    break;
+                // case "LE":
+                //     for(String d : binST.keys()) {
+                //         if(binST.get(d).getMarks() <= Double.parseDouble(str[1]))
+                //         System.out.println(binST.get(d).getName());
+                //     }
+                //     break;
+                // case "GE":
+
+                //     for(String d : binST.keys()) {
+                //         if(binST.get(d).getMarks() >= Double.parseDouble(str[1]))
+                //             System.out.println(binST.get(d).getName());
+                //     }
+                //     break;
             }
 
         }
