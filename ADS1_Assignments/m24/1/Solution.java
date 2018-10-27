d5cc59da61f030174c3929f7b3dad971d16ba4cb
nemnous <nani.botta.123@msitprogram.net>
2018-10-27 11:05:44 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS1_Assignments/m24/1/Solution.java
+++ ADS1_Assignments/m24/1/Solution.java
@@ -1,55 +1,135 @@
-import java.util.Scanner;
-import java.util.*;
-class Student {
-	String name;
-	int rollNumber;
-	Double marks;
+/**
+ * this problem uses Linear Probing hash Symbol.
+ * table.
+ * The time complexity of Linear Probing ST in worst case is:
+ * for Search O(n)
+ * for Insert O(n)
+ * for Delete O(n).
+ * The time complexity of Linear Probing ST in average case is:
+ * the insertion deletion and search has 1. with Uniform hashing assumption.
+ *
+ */
 
-	Student(int rollNumber, String name, Double marks) {
-		this.name = name;
-		this.rollNumber = rollNumber;
-		this.marks = marks;
-	} 
+import java.util.Scanner;
+
+/**
+ * Class for student.
+ * this class  contains the student information.
+ * Such as roll, name and marks.
+ */
+class Student {
+    /**
+     * variables name, roll, marks.
+     */
+    private String name;
+    /**
+     * roll number.
+     */
+    private int rollNumber;
+    /**
+     * marks.
+     */
+    private Double marks;
+
+    /**
+     * Constructs the object.
+     *
+     * @param      roll  The roll
+     * @param      nme   The nme
+     * @param      mrks  The mrks
+     */
+    Student(final int roll, final String nme, final Double mrks) {
+        this.name = nme;
+        this.rollNumber = roll;
+        this.marks = mrks;
+    }
+    /**
+     * Gets the name.
+     *
+     * @return     The name.
+     */
+    String getName() {
+        return this.name;
+    }
+    /**
+     * Gets the marks.
+     *
+     * @return     The marks.
+     */
+    Double getMarks() {
+        return this.marks;
+    }
+    /**
+     * Gets the roll.
+     *
+     * @return     The roll.
+     */
+    int getRoll() {
+        return this.rollNumber;
+    }
 }
 
 
+/**
+ * Class for solution.
+ * this is the solution class.
+ * handles the main function.
+ */
+final class Solution {
 
-class Solution {
-	public static void main(String[] args) {
-		LinearProbingHashST<Integer, Student > linearST = new LinearProbingHashST<Integer, Student>();
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * unused constructor
+         */
+    }
+    /**
+     * this is main fucntion and this
+     * handles the input from the cosole aned
+     * displays the output to the console.
+     * The time complexity of the main function is O(N).
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        LinearProbingHashST<Integer, Student> linearST
+            = new LinearProbingHashST<Integer, Student>();
 
-		Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-		int testCases = scan.nextInt();
-		scan.nextLine();
+        int testCases = scan.nextInt();
+        scan.nextLine();
 
-		for(int i = 0; i < testCases; i++) {
-			String[] temp = scan.nextLine().split(",");
+        for (int i = 0; i < testCases; i++) {
+            String[] temp = scan.nextLine().split(",");
+            final Double marks = Double.parseDouble(temp[2]);
 
-			Student stu = new Student(Integer.parseInt(temp[0]), temp[1], Double.parseDouble(temp[2]));
+Student stu = new Student(Integer.parseInt(temp[0]), temp[1], marks);
 
-			linearST.put(Integer.parseInt(temp[0]), stu);
+            linearST.put(Integer.parseInt(temp[0]), stu);
 
-		}
+        }
 
-		int noQuery = scan.nextInt();
-		scan.nextLine();
-		for(int i = 0; i < noQuery; i++) {
-			String[] query = scan.nextLine().split(" ");
-			// System.out.println(Arrays.toString(query));
-			int roll = Integer.parseInt(query[1]);
-			if(!linearST.contains(roll)) {
-				System.out.println("Student doesn't exists...");
-				continue;
-			}
-			switch(query[2]) {
-				case "1":
-					System.out.println(linearST.get(roll).name);
-					break;
-				case "2":
-					System.out.println(linearST.get(roll).marks);
-
-			}
-		}
-	}
+        int noQuery = scan.nextInt();
+        scan.nextLine();
+        for (int i = 0; i < noQuery; i++) {
+            String[] query = scan.nextLine().split(" ");
+            int roll = Integer.parseInt(query[1]);
+            if (!linearST.contains(roll)) {
+                System.out.println("Student doesn't exists...");
+                continue;
+            }
+            switch (query[2]) {
+            case "1":
+                System.out.println(linearST.get(roll).getName());
+                break;
+            case "2":
+                System.out.println(linearST.get(roll).getMarks());
+            default:
+                break;
+            }
+        }
+    }
 }
